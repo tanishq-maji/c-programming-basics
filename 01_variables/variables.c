f506845005d910b94b2b3751172bd651dadd356b
Tanishq Maji <tanishqmaji.social@gmail.com>
2026-09-19 00:45:39 +0530

    Add variables and data types example

---
+++ 01_variables/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int age = 19;
+    float height = 1.82;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Height: %.2f meters\n", height);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
